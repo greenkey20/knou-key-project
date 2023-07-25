82020d7a04fff2cae45762413a1483bfb3db5638
Eunyoung KANG <greenkey20@gmail.com>
2023-07-25 21:14:02 +0900

    feat - #25: 활동 계획 달력 JSP 및 자료형 구현

--- src/main/java/org/knou/keyproject/KeyProjectApplication.java
+++ src/main/java/org/knou/keyproject/KeyProjectApplication.java
@@ -2,7 +2,9 @@ package org.knou.keyproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 //@EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class KeyProjectApplication {
 		SpringApplication.run(KeyProjectApplication.class, args);
 	}
 
+	@Bean
+	MappingJackson2JsonView jsonView() {
+		return new MappingJackson2JsonView();
+	}
 }
