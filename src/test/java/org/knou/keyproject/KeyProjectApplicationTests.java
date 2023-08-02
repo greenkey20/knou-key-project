b2a8506043571537587f6228c6df71ce8cde89de
Eunyoung KANG <greenkey20@gmail.com>
2023-08-03 02:25:16 +0900

    docs - bootWar 빌드를 위한 코드 추가

--- src/test/java/org/knou/keyproject/KeyProjectApplicationTests.java
+++ src/test/java/org/knou/keyproject/KeyProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package org.knou.keyproject;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KeyProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package org.knou.keyproject;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class KeyProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
