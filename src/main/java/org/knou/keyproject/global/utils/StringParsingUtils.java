5a3374d346d63e767a9fbb2db2e543dc411fc863
Eunyoung KANG <greenkey20@gmail.com>
2023-08-23 18:02:14 +0900

    feat - 문자열을 <BR>로 split하는 공통 메서드 별도로 만듦

--- src/main/java/org/knou/keyproject/global/utils/StringParsingUtils.java
+++ src/main/java/org/knou/keyproject/global/utils/StringParsingUtils.java
@@ -9,4 +9,9 @@ public class StringParsingUtils {
         log.info("개행을 br 태그로 파싱 = " + memo.replaceAll("\r\n", "<br>"));
         return memo.replaceAll("\r\n", "<br>");
     }
+
+    // 2023.8.23(수) 16h40 ChatGPT 답변 활용 구현하다가 만들어봄
+    public static String[] splitIntoLinesByBr(String content) {
+        return content.split("<BR>");
+    }
 }
